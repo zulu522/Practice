9a3ef67a00c0c2bdb31469dad4d6bb29b29d8a46
zulu <bushuychenko55@gmail.com>
2022-06-25 23:06:56 +0300

    Main

--- Basics_w3resources/src/Ex52_53_54.java
+++ Basics_w3resources/src/Ex52_53_54.java
@@ -4,11 +4,14 @@
 // if the second number is greater than first number and third number is greater than second number.
 // If "abc" is true second number does not need to be greater than first number.
 
+//54: Write a Java program that accepts three integers from the user and
+// return true if two or more of them (integers ) have the same rightmost digit.
+// The integers are non-negative.
 
 import java.util.Scanner;
 
 public class Ex52_53_54 {
-    static boolean calculate() {
+    static boolean calculateEx52() {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Input the first number :");
@@ -21,7 +24,7 @@ public class Ex52_53_54 {
         return num3 == num1 + num2;
     }
 
-    static boolean calculate1() {
+    static boolean calculateEx53() {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Input the first number :");
@@ -35,11 +38,26 @@ public class Ex52_53_54 {
         return num2 > num1 && num3 > num2;
     }
 
+    static boolean calculateEx54(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input the first number :");
+        int num1 = scan.nextInt();
+        System.out.println("Input the second number :");
+        int num2 = scan.nextInt();
+        System.out.println("Input the third number :");
+        int num3 = scan.nextInt();
+
+        return (num1 % 10 == num2 % 10) || (num2 % 10 == num3 % 10) || (num3 % 10 == num1 % 10);
+    }
+
         public static void main (String[]args){
             //boolean a = calculate();
             //System.out.println(a);
 
-            boolean b = calculate1();
-            System.out.println(b);
+            //boolean b = calculateEx52();
+            //System.out.println(b);
+
+            boolean c = calculateEx54();
+            System.out.println(c);
         }
     }
